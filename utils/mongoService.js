cd92493de307f767b2c3a1cc0b840e37c6423c3b
akashi7 <christiannseko@gmail.com>
2024-10-23 12:42:20 +0200

    api auth

--- utils/mongoService.js
+++ utils/mongoService.js
@@ -1,20 +1,27 @@
-const { prisma } = require('../config/mongodb')
+const axios = require('axios');
+
+const url = `http://192.168.160.190/api/v1/auth/authenticate`;
 
 const AuthenticateUser = async (options) => {
-  const clientId = options.clientId
-  const clientSecret = options.clientSecret
+  const clientId = options.clientId;
+  const clientSecret = options.clientSecret;
 
-  const userCredentials = await prisma.userCredentials.findFirst({
-    where: {
-      AND: [{ clientId }, { clientSecret }],
-    },
-  })
+  try {
+    const response = await axios.post(url, {
+      clientId: clientId,
+      clientSecret: clientSecret,
+    });
 
-  if (!userCredentials) {
-    return null
+    if (response.data && response.data.userId) {
+      return response.data.userId;
+    }
+
+    return null;
+
+  } catch (error) {
+    console.error('Error authenticating user:', error.message);
+    return null;
   }
+};
 
-  return userCredentials.userId
-}
-
-module.exports = { AuthenticateUser }
+module.exports = { AuthenticateUser };
